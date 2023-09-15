10927008affebdad6edc6f4cbe1cd3299d9b6e2a
DanielBarreto01 <125820921+DanielBarreto01@users.noreply.github.com>
2023-09-15 09:04:10 -0500

    congruenciales and distribuin normal

--- NumerosPseudoaleatorios/Methods.py
+++ NumerosPseudoaleatorios/Methods.py
@@ -1,5 +1,10 @@
+import random
+import numpy as np
+import time
+
 class Methods:
 
+    
     def cuadrados_medios(self, semilla, iteraciones):
         numeros = []
         tam1 = len(semilla)
@@ -14,7 +19,30 @@ class Methods:
                 print("{}.  {}".format(i, numeros))
                 numero1 = int(numeros)
 # Crear una instancia de la clase Methods
+
+    def distribucion_normal(self,a,b,n):
+
+        print("Distribución Normal")
+        random_array = np.random.uniform(a, b, n)
+        pass
+
+        for i in range(n):
+            print("{}.  {}".format(i, random_array[i]))
+     
+        #toma la fecha para genrar el numero pseudoaleatorio   
+    def congruenciales(self,n):
+        print("Congruenciales")
+        xn = int(time.time())
+        for i in range(n):
+             xn1 = ((1103515245 * xn + 12345) % 32768) / 32768.00
+             print(xn1)
+             xn = xn1
+
 methods_instance = Methods()
+
+
 
 # Llamar al método cuadrados_medios a través de la instancia
 methods_instance.cuadrados_medios("7543", 10)
+methods_instance.distribucion_normal(0,1,10)
+methods_instance.congruenciales(10)
