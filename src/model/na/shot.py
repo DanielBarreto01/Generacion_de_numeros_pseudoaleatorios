e8a28bf683838e7985eded907bb7bc9ba7601c7a
joseAlexnander23 <jose231101a@gmail.com>
2023-09-15 21:43:44 -0500

    clase gender y shot.py

---
+++ src/model/na/shot.py
@@ -0,0 +1,25 @@
+from src.model import middle_square
+from src.model.na.Gender import Gender
+
+
+class Shot:
+    def __init__(self, codigo):
+        self.generator = middle_square(0, 1);
+        self.this.code = codigo;
+        self.gender = self.generate_gender();
+        self.initial_resistance = self.generate_resistance();
+        self.resistance = self.initial_resistance;
+        self.experience = 10;
+        self.luck = self.generateLuck();
+        self.points = 0;
+        self.roundPoints = 0;
+        self.wonRounds = 0;
+        self.wonRaffles = 0;
+        self.consecutive_won_raflle_number_round = -1;
+        self.countLuck = 0;
+
+    def generate_gender(self):
+        return Gender.MALE if self.generator.generateNi() >= 0.5 else Gender.FEMALE
+
+
+
