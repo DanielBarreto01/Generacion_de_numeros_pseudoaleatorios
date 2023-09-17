1d550c7fab5a124adbc3ed8b87b58476669ec354
Cristian Fernando Becerra Guerrero <cfbg99@hotmail.es>
2023-09-17 04:00:54 -0500

    GUI Tests
    Implemented GUI

--- Test/chi-cuadrado.py
+++ Test/chi-cuadrado.py
@@ -4,6 +4,8 @@ from scipy.stats import ksone
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import math
+import tkinter as tk
+from tkinter import ttk
 
 # Ingresa tus valores de conjunto de datos
 data = [0.9634731371579395, 0.8846470279646376, 0.7266874660722045, 0.35868355450366385, 0.3402934242066663, 0.2699767328916134, 0.31722243341888456, 0.5614001017785968, 0.8342195117324919, 0.7615286765340064]
@@ -11,6 +13,9 @@ total_datos = data.__len__()
 #El numero de intervalos es igual a la raiz cuadrada del numero de datos
 m = math.sqrt(total_datos)
 num_intervals = round(m)
+
+root = tk.Tk()
+root.title("Tabla Chi Cuadrado")
 
 #Ei = numero elementos / numero intervalos
 ei = total_datos / num_intervals
@@ -53,8 +58,8 @@ squared_difference = [((ei - observed) ** 2) / ei for observed in frequency]
 
 data_table = pd.DataFrame({
     'Int': list(range(1, num_intervals + 1)),
-    'Inicio del Intervalo': [interval[0] for interval in intervals],
-    'Fin del Intervalo': [interval[1] for interval in intervals],
+    'Inicial': [interval[0] for interval in intervals],
+    'Final': [interval[1] for interval in intervals],
     'Oi': frequency,
     'Ei': [ei] * num_intervals,
     '(Ei-Oi)^2/Ei': squared_difference
@@ -73,3 +78,35 @@ if(total < critical_value):
     print ("No se rechaza la hipotesis nula")
 else:
     print ("Se rechaza la hipotesis nula")
+
+table = ttk.Treeview(root, columns=list(data_table.columns), show="headings")
+table.column("Int", width="50")
+table.column("Inicial", width="100")
+table.column("Final", width="100")
+table.column("Oi", width="60")
+table.column("Ei", width="100")
+table.column("(Ei-Oi)^2/Ei", width="150")
+
+# Configurar encabezados de columnas
+for col in data_table.columns:
+    table.heading(col, text=col)
+
+# Agregar filas de datos a la tabla
+for i, row in data_table.iterrows():
+    table.insert('', 'end', values=list(row))
+
+# Colocar la tabla en la ventana
+table.grid(row=0, column=0, padx=10, pady=10)
+
+# ... (tu código existente) ...
+
+# Función para cerrar la ventana cuando se haga clic en el botón de "Cerrar"
+def cerrar_ventana():
+    root.destroy()
+
+# Botón para cerrar la ventana
+boton_cerrar = ttk.Button(root, text="Cerrar", command=cerrar_ventana)
+boton_cerrar.grid(row=1, column=0, padx=10, pady=10)
+
+# Ejecutar la aplicación de Tkinter
+root.mainloop()
